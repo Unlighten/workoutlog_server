7a23ce346939723c265ca5e7bb90bbf47dd051b0
Aaron Moore <x2kicker2x@gmail.com>
2017-10-06 09:46:03 -0400

    Maybe fixed something

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres'
-});
-
-// var sequelize = new Sequelize('workoutlog','postgres','edbpostgres2legacy2', {
-// 	host: 'localhost',
+// var sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:edbpostgres2legacy2@localhost:5432/workoutlog', {
 // 	dialect: 'postgres'
-// })
+// });
+
+var sequelize = new Sequelize('workoutlog','postgres','edbpostgres2legacy2', {
+	host: 'localhost',
+	dialect: 'postgres'
+})
 
 sequelize.authenticate().then(
 	function() {
